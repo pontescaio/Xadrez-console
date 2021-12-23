b81faea7e0e7437c1d7652e78988563eb59c9d45
Caio Pontes <caiopontesgs15@gmail.com>
2021-12-23 13:45:03 -0300

    Classe Posição criada

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace Xadrez_console
 {
@@ -6,7 +7,9 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
